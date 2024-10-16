b31850261630a425619ce196baba2a7f600518f3
Skycheeck <Sergeyxow@yandex.ru>
2024-10-16 17:21:20 +0300

    UniRx integration

--- quantum_unity/Assets/Scripts/HUDPresenter.cs
+++ quantum_unity/Assets/Scripts/HUDPresenter.cs
@@ -1,5 +1,7 @@
-﻿using Quantum;
+﻿using System;
+using Quantum;
 using Quantum.Commands;
+using UniRx;
 using UnityEngine;
 using Button = UnityEngine.UI.Button;
 
@@ -10,13 +12,38 @@ public class HUDPresenter : MonoBehaviour
     [SerializeField] private Button _upgradeButton;
     
     private UpgradePlayerCommand _upgradePlayerCommand;
+    
+    private IObservable<Unit> _upgradeButtonClickedObservable;
+    private Subject<PlayerModel> _playerModelUpdatedObservable;
+    
+    private IDisposable _upgradeButtonClickedSubscription;
+    private IDisposable _playerModelUpdatedSubscription;
 
-    private void Start()
+    private void Awake()
     {
-        QuantumEvent.Subscribe(this, (EventPlayerModelUpdatedEvent e) => OnPlayerModelUpdated(e.Model));
-        
         _upgradePlayerCommand = new UpgradePlayerCommand();
-        _upgradeButton.onClick.AddListener(() => QuantumRunner.Default.Game.SendCommand(_upgradePlayerCommand));
+        _playerModelUpdatedObservable = new Subject<PlayerModel>();
+        _upgradeButtonClickedObservable = _upgradeButton.onClick.AsObservable();
+    }
+
+    private void Start() => QuantumEvent.Subscribe(this, (EventPlayerModelUpdatedEvent e) => _playerModelUpdatedObservable.OnNext(e.Model));
+    private void OnDestroy() => QuantumEvent.UnsubscribeListener(this);
+
+    private void OnEnable()
+    {
+        _upgradeButtonClickedSubscription = _upgradeButtonClickedObservable.Subscribe(OnUpgradeButtonClicked);
+        _playerModelUpdatedSubscription = _playerModelUpdatedObservable.Subscribe(OnPlayerModelUpdated);
+    }
+
+    private void OnDisable()
+    {
+        _upgradeButtonClickedSubscription.Dispose();
+        _playerModelUpdatedSubscription.Dispose();
+    }
+
+    private void OnUpgradeButtonClicked(Unit obj)
+    {
+        QuantumRunner.Default.Game.SendCommand(_upgradePlayerCommand);
     }
 
     private void OnPlayerModelUpdated(PlayerModel model)
