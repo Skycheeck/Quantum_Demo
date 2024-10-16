e9fd7b8f6b8b53cd4270b6cbc55ecd1f0ce17f3e
Skycheeck <Sergeyxow@yandex.ru>
2024-10-16 16:51:58 +0300

    AttackSystem for verified frames only

--- quantum_code/quantum.code/Systems/AttackSystem.cs
+++ quantum_code/quantum.code/Systems/AttackSystem.cs
@@ -12,6 +12,7 @@ public unsafe class AttackSystem : SystemMainThreadFilter<AttackSystem.Filter>
         public EntityRef Entity;
         public Transform3D* Transform3D;
         public Attacker* Attacker;
+        public PlayerLink* PlayerLink;
     }
 
     public override void OnEnabled(Frame f)
@@ -28,19 +29,27 @@ public unsafe class AttackSystem : SystemMainThreadFilter<AttackSystem.Filter>
 
     public override void Update(Frame f, ref Filter filter)
     {
+        if (!f.IsVerified) return;
+        
         f.Physics3D.OverlapShape(_persistentHitCollection3D, filter.Transform3D->Position,
             filter.Transform3D->Rotation, Shape3D.CreateSphere(filter.Attacker->Radius), filter.Attacker->Mask);
 
         for (int i = 0; i < _persistentHitCollection3D->Count; i++)
         {
             EntityRef entityRef = _persistentHitCollection3D->HitsBuffer[i].Entity;
-            if (f.Has<Dead>(entityRef)) continue;
+            if (!f.Unsafe.TryGetPointer(entityRef, out Health* health)) continue; // skip if no health
+            if (f.Has<Dead>(entityRef) || health->Current <= FP._0) continue; // skip if dead
 
-            if (!f.Unsafe.TryGetPointer(entityRef, out Health* health)) continue;
-            health->Current -= filter.Attacker->DPS * f.DeltaTime;
+            FP damage = filter.Attacker->DPS * f.DeltaTime;
+            health->Current -= damage; // do damage
 
-            if (health->Current > FP._0) continue;
-            f.Add<Dead>(entityRef);
+            if (health->Current > FP._0) continue; // skip if damage is not enough to kill
+            f.Add<Dead>(entityRef); // add dead
+
+            // update kill counter and send event to view
+            RuntimePlayer runtimePlayer = f.GetPlayerData(filter.PlayerLink->Player);
+            runtimePlayer.PlayerModel.EnemiesKilled++;
+            f.Events.PlayerModelUpdatedEvent(runtimePlayer.PlayerModel);
         }
 
         _persistentHitCollection3D->Reset();
